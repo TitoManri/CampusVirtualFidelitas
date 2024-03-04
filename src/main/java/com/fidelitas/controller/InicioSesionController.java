0acbbb5d502d1663d5e9069c7e970cb7ad62ebff
Tito <61942255+TitoManri@users.noreply.github.com>
2024-03-03 20:30:49 -0600

    Inicio de Sesion
    HTML y CSS listo

--- src/main/java/com/fidelitas/controller/InicioSesionController.java
+++ src/main/java/com/fidelitas/controller/InicioSesionController.java
@@ -13,30 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class InicioSesionController {
-
-    private final InicioSesionService inicioSesionService;
-
+    
     @Autowired
-    public InicioSesionController(InicioSesionService inicioSesionService) {
-        this.inicioSesionService = inicioSesionService;
-    }
+    private InicioSesionService inicioSesionService;
 
-    @GetMapping("/iniciosesion")
+    @GetMapping("/")
     public String mostrarPaginaInicioSesion(Model model) {
-        List<InicioSesion> estudiantes = inicioSesionService.getInicioSesiones(true);
-        model.addAttribute("estudiantes", estudiantes);
-        model.addAttribute("inicioSesion", new InicioSesion()); // Agrega esto
-
-        return "iniciosesion/index";
-    }
-
-
-    @PostMapping("/login")
-    public String procesarInicioSesion(@ModelAttribute InicioSesion inicioSesion, Model model) {
-        // Lógica para procesar el inicio de sesión y operaciones en la base de datos
-        // Puedes llamar a métodos de inicioSesionService aquí para manejar la base de datos
-
-        // Después de procesar el inicio de sesión, podrías redirigir a otra página o simplemente volver a mostrar la misma página
-        return "redirect:/dashboard";  // Redirige a otra página después de procesar el inicio de sesión
+        model.addAttribute("inicioSesion", new InicioSesion());
+        return "/iniciosesion/login";
     }
 }
