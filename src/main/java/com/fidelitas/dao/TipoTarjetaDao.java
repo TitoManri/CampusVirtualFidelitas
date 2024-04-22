198dcb7f77c0bd524ff4a0aed924c142493c2d46
FlashNi2202 <nigelocampo22@gmail.com>
2024-04-21 20:27:09 -0400

    feat: add findByNombre method to TipoTarjetaDao.java

--- src/main/java/com/fidelitas/dao/TipoTarjetaDao.java
+++ src/main/java/com/fidelitas/dao/TipoTarjetaDao.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TipoTarjetaDao extends JpaRepository <TipoTarjeta, Long>{
     TipoTarjeta findById(long id);
+
+    TipoTarjeta findByNombre(String tipoTarjeta);
 }
