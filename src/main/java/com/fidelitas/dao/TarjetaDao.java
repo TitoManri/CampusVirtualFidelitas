5c1c36d0fb841d5aa0c057785ecf5e03fbd9f06e
FlashNi2202 <nigelocampo22@gmail.com>
2024-04-21 20:26:41 -0400

    feat: add findByIdTarjeta and findByNumero methods to TarjetaDao.java

--- src/main/java/com/fidelitas/dao/TarjetaDao.java
+++ src/main/java/com/fidelitas/dao/TarjetaDao.java
@@ -4,5 +4,7 @@ import com.fidelitas.domain.Tarjeta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TarjetaDao extends JpaRepository <Tarjeta, Long> {
-    Tarjeta findById(long id);
+    Tarjeta findByIdTarjeta(long id);
+
+    Tarjeta findByNumero(String numeroTarjeta);
 }
